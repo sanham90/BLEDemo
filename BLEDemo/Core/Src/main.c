57e91cd8c1cf3065b39317f202e97e083ae08a24
sanham90 <d.sanham@gmail.com>
2021-01-17 12:29:50 +0000

    Actual Addition of the REFDES117 Libraries for HRM and POM
    Commentd out mbed.h as will use I2C libraries of stm32

--- BLEDemo/Core/Src/main.c
+++ BLEDemo/Core/Src/main.c
@@ -23,7 +23,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "algorithm.h"
+#include "MAX30102.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
